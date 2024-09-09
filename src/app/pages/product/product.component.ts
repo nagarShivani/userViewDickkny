8257bee20370532f3e78b3d411568dd494b5ddff
nagarShivani <nagarnanika@gmail.com>
2024-09-09 12:40:44 +0530

    updating ts file

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
@@ -9,31 +9,34 @@ import { UserService } from 'src/app/services/user/user.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   quantity: number = 1;
   productDetails: any;
   productId: any;
   isLoggedInObject: any;
   selectedSize: any;
   getAllProducts: any;
+
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
     private loaderService: LoaderService,
     private router: Router,
     private categoryService: CategoryService
-  ) {
+  ) {}
+
+  ngOnInit() {
     const allProducts = localStorage.getItem('trendingProducts');
     if (allProducts) {
       this.getAllProducts = JSON.parse(allProducts);
     }
     this.route.params.subscribe((params) => {
       this.productId = params['id'];
-      this.getproductByID(this.productId);
+      this.getProductByID(this.productId);
     });
   }
 
-  getproductByID(id: any) {
+  getProductByID(id: any) {
     this.loaderService.showLoading();
     this.categoryService.getProductByID(id).subscribe(
       (res: any) => {
@@ -42,57 +45,65 @@ export class ProductComponent {
       },
       (error: any) => {
         this.loaderService.hideLoading();
+        console.error('Error fetching product details:', error);
         this.userService.toast.snackbarError(error.error.error);
       }
     );
   }
 
   addToWishlist(productID: any) {
+    if (!this.isLoggedInObject) {
+      this.router.navigate(['signin']);
+      this.userService.toast.snackbarError('Please first login or register');
+      return;
+    }
+
     let payload = {
       userId: this.isLoggedInObject.loginid,
       productId: productID,
     };
+
     this.categoryService.addToWishlist({ body: payload }).subscribe(
       (res: any) => {
         this.userService.toast.snackbarError(res.message);
         this.categoryService.isCartandWishlistCountCheck(true);
       },
       (error: any) => {
+        console.error('Error adding to wishlist:', error);
         this.userService.toast.snackbarError(error.error.error);
       }
     );
   }
 
   addToCart(productID: any) {
-    const storedUserInfo = localStorage.getItem('isLoggedIn');
-    if (storedUserInfo) {
-      try {
-        this.isLoggedInObject = JSON.parse(storedUserInfo);
-        let payload = {
-          userId: this.isLoggedInObject.loginid,
-          productId: productID,
-          quantity: this.quantity,
-          size: this.selectedSize._id,
-        };
-        this.categoryService.addToCart({ body: payload }).subscribe(
-          (res: any) => {
-            this.userService.toast.snackbarError(res.message);
-            this.categoryService.isCartandWishlistCountCheck(true);
-          },
-          (error: any) => {
-            this.userService.toast.snackbarError(error.error.error);
-          }
-        );
-      } catch (error) {
-        this.isLoggedInObject = {};
-        this.router.navigate(['signin']);
-        this.userService.toast.snackbarError('Please first login or register');
-      }
-    } else {
-      this.isLoggedInObject = {};
+    if (!this.isLoggedInObject) {
       this.router.navigate(['signin']);
       this.userService.toast.snackbarError('Please first login or register');
+      return;
     }
+
+    let payload = {
+      userId: this.isLoggedInObject.loginid,
+      productId: productID,
+      quantity: this.quantity,
+      size: this.selectedSize ? this.selectedSize._id : null,
+    };
+
+    if (!payload.size) {
+      this.userService.toast.snackbarError('Please select a size');
+      return;
+    }
+
+    this.categoryService.addToCart({ body: payload }).subscribe(
+      (res: any) => {
+        this.userService.toast.snackbarError(res.message);
+        this.categoryService.isCartandWishlistCountCheck(true);
+      },
+      (error: any) => {
+        console.error('Error adding to cart:', error);
+        this.userService.toast.snackbarError(error.error.error);
+      }
+    );
   }
 
   selectSize(productSize: any) {
