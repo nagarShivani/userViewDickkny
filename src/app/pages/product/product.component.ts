ff5a2a92e0eeee317bcf01535dd9075447887f1d
nagarShivani <nagarnanika@gmail.com>
2024-09-11 12:21:36 +0530

    new arrival showing four products

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
@@ -9,37 +9,51 @@ import { UserService } from 'src/app/services/user/user.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   quantity: number = 1;
   productDetails: any;
+  imgArray: any[] = [];
   productId: any;
   isLoggedInObject: any;
   selectedSize: any;
   getAllProducts: any;
   sizeSelected: boolean = false;
+  apiUrl: string = 'your_api_url_here'; // Make sure to define your apiUrl
+
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
     private loaderService: LoaderService,
     private router: Router,
     private categoryService: CategoryService
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     const allProducts = localStorage.getItem('trendingProducts');
     if (allProducts) {
       this.getAllProducts = JSON.parse(allProducts);
     }
+
     this.route.params.subscribe((params) => {
       this.productId = params['id'];
-      this.getproductByID(this.productId);
+      if (this.productId) {
+        this.getproductByID(this.productId);
+      }
     });
   }
 
-  getproductByID(id: any) {
+  getproductByID(id: any): void {
     this.loaderService.showLoading();
+
     this.categoryService.getProductByID(id).subscribe(
       (res: any) => {
-        console.log('product respnseeeee', res);
+        console.log('response 1', res);
         this.productDetails = res;
+        console.log('response 2', this.productDetails);
+
+        this.imgArray = this.productDetails.multipleimage || [];
+        console.log('response 2', this.imgArray);
+
         this.loaderService.hideLoading();
       },
       (error: any) => {
@@ -49,9 +63,9 @@ export class ProductComponent {
     );
   }
 
-  addToWishlist(productID: any) {
-    let payload = {
-      userId: this.isLoggedInObject.loginid,
+  addToWishlist(productID: any): void {
+    const payload = {
+      userId: this.isLoggedInObject?.loginid,
       productId: productID,
     };
     this.categoryService.addToWishlist({ body: payload }).subscribe(
@@ -65,65 +79,34 @@ export class ProductComponent {
     );
   }
 
-  // addToCart(productID: any) {
-  //   const storedUserInfo = localStorage.getItem('isLoggedIn');
-  //   if (storedUserInfo) {
-  //     try {
-  //       this.isLoggedInObject = JSON.parse(storedUserInfo);
-  //       let payload = {
-  //         userId: this.isLoggedInObject.loginid,
-  //         productId: productID,
-  //         quantity: this.quantity,
-  //         size: this.selectedSize._id,
-  //       };
-  //       this.categoryService.addToCart({ body: payload }).subscribe(
-  //         (res: any) => {
-  //           this.userService.toast.snackbarError(res.message);
-  //           this.categoryService.isCartandWishlistCountCheck(true);
-  //         },
-  //         (error: any) => {
-  //           this.userService.toast.snackbarError(error.error.error);
-  //         }
-  //       );
-  //     } catch (error) {
-  //       this.isLoggedInObject = {};
-  //       this.router.navigate(['signin']);
-  //       this.userService.toast.snackbarError('Please first login or register');
-  //     }
-  //   } else {
-  //     this.isLoggedInObject = {};
-  //     this.router.navigate(['signin']);
-  //     this.userService.toast.snackbarError('Please first login or register');
-  //   }
-  // }
-  addToCart(productID: any) {
-    if (!this.isLoggedInObject.loginid) {
+  addToCart(productID: any): void {
+    if (!this.isLoggedInObject?.loginid) {
       this.userService.toast.snackbarError('Please First Login.');
       return;
     }
-    let payload = {
+    const payload = {
       userId: this.isLoggedInObject.loginid,
       productId: productID,
-      quantity: 1,
+      quantity: this.quantity,
       size: this.selectedSize?._id,
     };
     this.categoryService.addToCart({ body: payload }).subscribe(
       (res: any) => {
-        console.log('resss');
+        console.log('Add to cart response', res);
         this.selectedSize = null;
         this.sizeSelected = false;
         this.categoryService.isCartandWishlistCountCheck(true);
         this.userService.toast.snackbarError(res.message);
       },
       (error: any) => {
-        console.log('error');
-
+        console.log('Add to cart error', error);
         this.userService.toast.snackbarError(error.error.error);
       }
     );
   }
-  selectSize(productSize: any) {
+
+  selectSize(productSize: any): void {
     this.selectedSize = productSize;
-    // this.sizeSelected = true;
+    // this.sizeSelected = true; // Uncomment if you need to use this
   }
 }
